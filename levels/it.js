541039f42817b462cd8e66305d3c6344403450ef
mgarzia3 <marco.a.garzia@gmail.com>
2026-02-23 09:29:04 +0000

    Level 3 platforms

--- levels/it.js
+++ levels/it.js
@@ -4,33 +4,66 @@ const WORLD_WIDTH = 2000;
 const WORLD_HEIGHT = 1000;
 
 export class ITLesson extends Phaser.Scene {
-  constructor(...args) {
-    super({ key: "ITLesson", ...args });
+
+  constructor( ...args ) {
+    super({ key: 'ITLesson', ...args })
   }
 
   preload() {
     this.load.atlas("player", "assets/player.png", "assets/player.json");
-    this.load.image("it-classroom", "assets/it-classroom.png");
+    this.load.image("it-classroom", "assets/it-classroom.jpg");
+    this.load.image("monitor", "assets/monitor.jpg");
+    this.load.image("table", "assets/table.png");
+    this.load.image("plank", "assets/plank.png");
   }
 
   create() {
-    // Settings
-    this.matter.world.setBounds(0, 0, WORLD_WIDTH, WORLD_HEIGHT);
-    this.cameras.main.setBounds(0, 0, WORLD_WIDTH, WORLD_HEIGHT);
-    this.cameras.main.setZoom(1.3);
+      // Settings
+      this.matter.world.setBounds(0, 0, WORLD_WIDTH, WORLD_HEIGHT);
+      this.cameras.main.setBounds(0, 0, WORLD_WIDTH, WORLD_HEIGHT);
+      this.cameras.main.setZoom(1.3);
+  
+      // Background
+      this.add.image(1456, 571, "it-classroom");
+      var player = createPlayer(this);
+      this.cameras.main.startFollow(player, true, 0.1, 0.1);  
+      this.cameras.main.setBackgroundColor("#ccccff");
 
-    // Background
-    this.add.image(1000, 500, "it-classroom").setScale(7);
-    var player = createPlayer(this);
-    this.cameras.main.startFollow(player, true, 0.1, 0.1);
-    this.cameras.main.setBackgroundColor("#ccccff");
+      // Platforms (static Matter bodies)
+      this.matter.add
+      .image(460, 870, "monitor", null, { isStatic: true })
+      .setScale(0.08);
+      this.matter.add
+      .image(430, 965, "table", null, { isStatic: true })
+      .setScale(0.15);
 
-    // World
+      // Moving Platforms
+      var movingPlank = this.matter.add
+      .image(466, 300, "plank")
+      .setIgnoreGravity(true);
 
-    // Platforms
-  }
+      var tween = scene.tweens.add({
+        targets: movingPlank,
+        x: 1,
+        x: '-=1',
+        // x: { from: 0, to: 1 },
+        // x: { start: 0, to: 1 },  
+        // x: { start: value0, from: value1, to: value2 },  
+        // x: {
+        //      getActive: function (target, key, value, targetIndex, totalTargets, tween) { return newValue; },
+        //      getStart: function (target, key, value, targetIndex, totalTargets, tween) { return newValue; },
+        //      getEnd: function (target, key, value, targetIndex, totalTargets, tween) { return newValue; }
+        // },
+        ease: 'Linear',       // 'Cubic', 'Elastic', 'Bounce', 'Back'
+        duration: 1000,
+        repeat: -1,            // -1: infinity
+        yoyo: true,
+
+        // interpolation: null,
+      });
+    }
 
   update(time, delta) {
-    movement();
+      movement();
   }
 }
--- levels/outside-school.js
+++ levels/outside-school.js
@@ -24,11 +24,8 @@ export class OutsideSchool extends Phaser.Scene {
 
     //Background
     this.add.image(1024, 500, "background");
-
     var player = createPlayer(this);
-
     this.cameras.main.startFollow(player, true, 0.1, 0.1);
-
     this.cameras.main.setBackgroundColor("#ccccff");
 
     // World
