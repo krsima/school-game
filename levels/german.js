b40d13da7be547962399be468f4a10c58de09378
Tamion <70228790+notTamion@users.noreply.github.com>
2026-03-02 09:24:50 +0000

    more work on level3

--- levels/german.js
+++ levels/german.js
@@ -11,6 +11,7 @@ export class GermanLesson extends Phaser.Scene {
   preload() {
     this.load.atlas("player", "assets/player.png", "assets/player.json");
     this.load.image("classroom", "assets/classroom.png");
+    this.load.image("teacher", "assets/teacher.jpg");
     this.load.image("chair", "assets/chair.jpg");
   }
 
@@ -22,6 +23,7 @@ export class GermanLesson extends Phaser.Scene {
 
     //Background
     this.add.image(1000, 500, "classroom").setScale(1.8);
+    this.add.image(1000, 700, "teacher").setScale(1.4);
 
     this.player = createPlayer(this);
     this.player.setPosition(1000, 500);
@@ -31,11 +33,24 @@ export class GermanLesson extends Phaser.Scene {
     this.cameras.main.setBackgroundColor("#ccccff");
 
     // World
+    this.timerText = this.make.text({
+      x: 1000,
+      y: 300,
+      text: "",
+      style: {
+        fontSize: "24px",
+        fontFamily: "Arial",
+        color: "#ffffff",
+        align: "center", // 'left'|'center'|'right'|'justify'
+      },
+      add: true,
+    });
+
 
     this.teacherspeach = this.make.text({
       x: 1000,
       y: 500,
-      text: "A und D zum Bewegen\nLeertaste zum Springen",
+      text: "",
       style: {
         fontSize: "24px",
         fontFamily: "Arial",
@@ -48,7 +63,7 @@ export class GermanLesson extends Phaser.Scene {
     // Chairs (dynamic Matter bodies)
     const chairPositions = [200, 400, 600, 900, 1100, 1400, 1600, 1800];
     this.chairs = chairPositions.map((x) => {
-      const chair = this.matter.add.image(x, 668, "chair");
+      const chair = this.matter.add.image(x, 750, "chair");
       chair.setScale(0.2).setBounce(0.5);
       return chair;
     });
@@ -56,10 +71,22 @@ export class GermanLesson extends Phaser.Scene {
     this.matter.world.on("collisionstart", (event) => {
       event.pairs.forEach((pair) => {
         const { bodyA, bodyB, collision } = pair;
-        if (bodyA.gameObject === this.player && this.chairs.includes(bodyB.gameObject) && bodyB.gameObject.body.velocity.y - bodyA.gameObject.body.velocity.y > 15) {
-          die();
+        if (
+          bodyB.gameObject === this.player &&
+          this.chairs.includes(bodyA.gameObject)
+        ) {
+          var bodyTmp = bodyA;
+          bodyA = bodyB;
+          bodyB = bodyTmp;
         }
-        if (bodyB.gameObject === this.player && this.chairs.includes(bodyA.gameObject) && bodyA.gameObject.body.velocity.y - bodyB.gameObject.body.velocity.y > 15) {
+        let velocity =
+          Math.pow(bodyB.velocity.y - bodyA.velocity.y, 2) +
+          Math.pow(bodyB.velocity.x - bodyA.velocity.x, 2);
+        if (
+          bodyA.gameObject === this.player &&
+          this.chairs.includes(bodyB.gameObject) &&
+          velocity > 400
+        ) {
           die();
         }
       });
@@ -67,9 +94,21 @@ export class GermanLesson extends Phaser.Scene {
 
     // Timetable
 
+    let timer = 120;
+
+    this.cur = false;
     this.time.addEvent({
       delay: 1000, // ms
       callback: () => {
+        this.timerText.setText(new Date(timer * 1000).toISOString().slice(14, 19));
+        timer--;
+        if (timer < 0) {
+          this.timerText.setText("GESCHAFFT!!!");
+          this.time.removeAllEvents();
+          this.time.delayedCall(2500, () => {
+            this.scene.start("ITLesson");
+          });
+        }
         if (Math.random() < 0.3 && !this.cur) {
           this.throwChairs();
         }
@@ -90,9 +129,21 @@ export class GermanLesson extends Phaser.Scene {
       if (Math.random() < 0.6) {
         return;
       }
+      var left = false;
+      var timer = this.time.addEvent({
+        delay: 20, // ms
+        callback: () => {
+          chair.setVelocity(left ? -10 : 10, 0);
+          left = !left;
+        },
+        callbackScope: this,
+        repeat: 40,
+      });
       this.cur = true;
-      chair.setVelocity(0, Math.max(0.8, Math.random()) * -45);
-      this.time.delayedCall(500, () => {
+      this.time.delayedCall(1000, () => {
+        chair.setVelocity(0, Math.max(0.8, Math.random()) * -45);
+      });
+      this.time.delayedCall(1400, () => {
         chair.setVelocityX(
           Math.ceil(Math.random() * 33) * (Math.round(Math.random()) ? 1 : -1),
         );
