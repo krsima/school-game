3ca0d3686e1eea24d241be3082918142db4a1dc4
mgarzia3 <marco.a.garzia@gmail.com>
2026-03-02 09:29:03 +0000

    Level 3 platforms

--- levels/it.js
+++ levels/it.js
@@ -34,28 +34,85 @@ export class ITLesson extends Phaser.Scene {
       .image(460, 870, "monitor", null, { isStatic: true })
       .setScale(0.08);
       this.matter.add
-      .image(430, 965, "table", null, { isStatic: true })
-      .setScale(0.15);
+      .image(650, 480, "monitor", null, { isStatic: true })
+      .setScale(0.09);
+      this.matter.add
+      .image(1250, 313, "monitor", null, { isStatic: true })
+      .setScale(0.075);
+      this.matter.add
+      .image(1400, 600, "monitor", null, { isStatic: true })
+      .setScale(0.08);
+      this.matter.add
+      .image(1480, 600, "monitor", null, { isStatic: true })
+      .setScale(0.08);
+      this.matter.add
+      .image(1560, 600, "monitor", null, { isStatic: true })
+      .setScale(0.08);
+      this.matter.add
+      .image(1850, 550, "plank", null, { isStatic: true })
+      .setScale(0.2);
+      this.matter.add
+      .image(1920, 550, "plank", null, { isStatic: true })
+      .setScale(0.2);
 
       // Moving Platforms
-      var movingPlank = this.matter.add
-      .image(466, 600, "plank")
+      var movingPlank1 = this.matter.add
+      .image(550, 750, "plank")
+      .setScale(0.2)
+      .setFixedRotation()
+      .setMass(1000)
+      .setIgnoreGravity(true);
+
+      var movingPlank2 = this.matter.add
+      .image(800, 300, "plank")
       .setScale(0.2)
       .setFixedRotation()
       .setMass(1000)
       .setIgnoreGravity(true);
 
       var tween = this.tweens.add({
-        targets: movingPlank,
-        x: [100, 300, 200, 600],
-        y: 600,
+        targets: movingPlank1,
+        x: 550,
+        y: 500,
         ease: 'Linear',       // 'Cubic', 'Elastic', 'Bounce', 'Back'
         duration: 3000,
+        repeat: -1,            // -1: infinity
+        yoyo: true
+      })
+
+      var tween = this.tweens.add({
+        targets: movingPlank2,
+        x: 1180,
+        y: 300,
+        ease: 'Linear',       // 'Cubic', 'Elastic', 'Bounce', 'Back'
+        duration: 4000,
         repeat: -1,            // -1: infinity
         yoyo: true,
 
         // interpolation: null,
       });
+
+      // Floor
+      this.matter.add
+      .image(430, 965, "table", null, { isStatic: true })
+      .setScale(0.15);
+      this.matter.add
+      .image(900, 965, "table", null, { isStatic: true })
+      .setScale(0.15);
+      this.matter.add
+      .image(1450, 965, "table", null, { isStatic: true })
+      .setScale(0.15);
+      this.matter.add
+      .image(1550, 965, "table", null, { isStatic: true })
+      .setScale(0.15);
+  
+
+      // Door (sensor for scene transition)
+      const door = this.matter.add.image(1920, 475, "door", null, {
+      isStatic: true,
+      isSensor: true,
+      });
+      door.setScale(0.1);
     }
 
   update(time, delta) {
